bc8d6c1153d8533f969a6be9ed6325ac99e3326d
rahscripts <rahmantjis@gmail.com>
2026-04-17 01:49:09 +0530

    get zenvoice btn component added

--- app/zenvoice/page.tsx
+++ app/zenvoice/page.tsx
@@ -1,13 +1,18 @@
-import LandingHeader from "./components/LandingHeader"
+import LandingHeader from "./components/LandingHeader";
+import GetZenvoiceBtn from "./components/ui/GetZenvoiceBtn";
 
 
 
 
 export default function ZenvoicePage() {
     return (
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-5xl mx-auto w-full">
             <LandingHeader />
-            zenvoice landing page  zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page zenvoice landing page
+            <section>
+                <h1 className="text-4xl font-bold">Zenvoice</h1>
+                <p className="mt-4">Zenvoice landing page content goes here.</p>
+                <GetZenvoiceBtn />
+            </section>
         </div>
     )
 }
