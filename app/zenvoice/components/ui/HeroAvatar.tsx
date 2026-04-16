c7313aa52ac3655afa6995e58212f82129bb6b1c
rahscripts <rahmantjis@gmail.com>
2026-04-17 02:10:10 +0530

    Landing page done

---
+++ app/zenvoice/components/ui/HeroAvatar.tsx
@@ -0,0 +1,48 @@
+export default function HeroAvatar() { 
+    return (
+          <div className="flex gap-2 mt-3">
+                    <div className="avatar-group -space-x-6">
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/wonderperson@192.webp" />
+    </div>
+  </div>
+   <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
+    </div>
+  </div>
+</div>
+<div className="flex flex-col items-start gap-2 ">
+<div className="rating">
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="1 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="2 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="3 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="4 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="5 star" defaultChecked/>
+</div>
+<div>
+    <p>
+        1675 founders sleep better
+    </p>
+</div>
+</div>
+                </div>
+                
+            
+)}
--- app/zenvoice/page.tsx
+++ app/zenvoice/page.tsx
@@ -1,17 +1,48 @@
 import LandingHeader from "./components/LandingHeader";
 import GetZenvoiceBtn from "./components/ui/GetZenvoiceBtn";
+import HeroAvatar from "./components/ui/HeroAvatar";
 
 
 
 
 export default function ZenvoicePage() {
+
+    const list = [
+       " 1-minute no-code setup",
+"Editable invoices (VAT, etc.)",
+"No 0.4% Stripe invoice fee"
+    ];
     return (
         <div className="max-w-5xl mx-auto w-full">
             <LandingHeader />
-            <section>
-                <h1 className="text-4xl font-bold">Zenvoice</h1>
-                <p className="mt-4">Zenvoice landing page content goes here.</p>
+            <section className="h-screen flex mt-30 items-center justify-start flex-col gap-6 text-center px-4">
+               <p><span className="text-xs text-gray-500">Made for</span> <span className="text-blue-600 font-semibold tracking-tighter">stripe</span></p>
+               <h1 className="text-6xl tracking-tight font-black max-w-2xl">
+               Stop paying <span className="italic text-red-500">0.4%</span> <br />
+            for Stripe invoices
+               </h1>
+               <p className="text-lg text-gray-600 max-w-xl">
+                Let your customers generate, edit, and download <br /> their invoices, with just<span className="underline italic decoration-emerald-600 decoration-2 decoration-wavy"> a link.</span>
+               </p>
+
+
+               <div className="flex flex-col items-start gap-2">
+                   {
+                    list.map((item, index) => (
+                        <div key={index} className="flex items-center gap-2">
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-4 text-green-500">
+                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                   </svg>
+                            <p className="text-gray-700">{item}</p>
+                        </div>
+                    ))}
+               </div>            
                 <GetZenvoiceBtn />
+                <HeroAvatar />
+
+
+
+              
             </section>
         </div>
     )
